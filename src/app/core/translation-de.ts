56a251c6c7023a97be11626328037fa573e0a3f7
Raghavendra Nayak <ragu556@gmail.com>
2020-07-25 13:13:55 +0530

    Update translation-de.ts

--- src/app/core/translation-de.ts
+++ src/app/core/translation-de.ts
@@ -1,7 +1,3 @@
-// Important Note:
-// Content ids should be used from confluence page:
-// https://d3.ce.capgemini.com/confluence/display/BNOTK/Content
-
 export const translationDe = {
   common: {
     buttons: {     
